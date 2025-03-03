d8da79b7880f966d978206cdaf3f73d5010ecd47
s1rojov <sirojoff2727@gmail.com>
2025-03-03 12:04:26 +0500

    fix:

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,16 @@
 import { Pool } from "pg";
 const pool: any = new Pool({
-  user: "koyeb-adm",
-  host: "ep-still-bush-a2z3hbxu.eu-central-1.pg.koyeb.app",
+  user: "koyeb",
+  host: "ep-lingering-cloud-a2t040wd.eu-central-1.pg.koyeb.app",
   database: "koyebdb",
-  password: "npg_28RGpWVemXqb",
-  port: 8000,
-  // ssl: true,
+  password: "npg_RnbokFOG4j8V",
+  port: 5432,
+  ssl: { rejectUnauthorized: false },
 });
+
+pool
+  .connect()
+  .then(() => console.log("PostgreSQL database connected! ✅"))
+  .catch((err: any) => console.error("Database connection error ❌", err));
 
 export default pool;
